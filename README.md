811d608ffa1875b96d1d23f75f28f0e5abcd412a
lubien <lubien1996@gmail.com>
2017-03-31 19:49:14 -0300

    Init

---
+++ README.md
@@ -0,0 +1,31 @@
+# Cerebro Plugin
+
+> Cerebro plugin that lets you start, stop and open [Hotel](https://www.npmjs.com/package/hotel) apps.
+
+## Usage
+
+In Cerebro, type `hotel` then:
+
+* Press <kbd>Tab</kbd> to toggle server status as running/stopped.
+* Press <kbd>Enter</kbd> to open the proxied URL.
+* Press <kbd>Shift+Enter</kbd> to open the real URL.
+
+## Settings
+
+This plugin assume your Hotel runs at `http://localhost:2000` and your TLD is 'dev'. To change this you need to edit the following envs:
+
+```sh
+HOTEL_HOST='http://127.0.0.1:2000'
+TLD='dev'
+```
+
+![](screenshot.png)
+
+## Related
+
+* [Cerebro](http://github.com/KELiON/cerebro) – main repo for Cerebro app;
+* [alfred-hotel](https://github.com/exah/alfred-hotel/) - My version is based on this.
+
+## License
+
+MIT © [Lubien](http://lubien.me)
---
+++ src/index.js
@@ -0,0 +1,80 @@
+'use strict';
+const
+  got = require('got')
+
+, icon = require('../icon.png')
+
+, {HOTEL_HOST = 'http://127.0.0.1:2000', TLD = 'dev'} = process.env
+
+, ENDPOINT = `${HOTEL_HOST}/_/servers`
+
+, TAB_CODE = 9
+
+, keyword = 'hotel'
+
+, pipe = (...fs) => y => fs.reduce((x, f) => f(x), y)
+
+, compose = (...fs) => pipe(...fs.reverse())
+
+, map = f => xs => xs.map(f)
+
+, prop = p => o => o[p]
+
+, serversToList = obj =>
+    Object.keys(obj).map(id => Object.assign({}, obj[id], {id}))
+
+, isRunning = status => status === 'running'
+
+, toggle = ({id, status}) =>
+    got.post(`${ENDPOINT}/${id}/${isRunning(status) ? 'stop' : 'start'}`)
+
+, serverToResult = scope => server => {
+    const
+      {id, status} = server
+    , running = isRunning(status)
+    , url = { http: `${HOTEL_HOST}/${id}`
+            , proxy: `http://${id}.${TLD}`
+            }
+
+    return {
+      icon
+    , id: url.proxy
+    , title: `${id} [${running ? 'On' : 'Off'}]`
+    , subtitle: 'Tab to toggle. Enter to open URL. Shift+Enter to open real URL.'
+    , clipboard: url.proxy
+    , onSelect(event) {
+        if (!running) toggle(server)
+        scope.actions.open(event.tabKey ? url.http : url.proxy)
+      }
+    , onKeyDown(event) {
+        if (event.keyCode !== TAB_CODE) return;
+        event.preventDefault()
+
+        toggle({id, status}).then(() => {
+          const
+            nextStatus = running ? 'stopped' : 'running'
+          , next = serverToResult(scope)({id, status: nextStatus})
+
+          scope.update(url.proxy, next)
+        })
+      }
+    }
+  }
+
+, fn = ({term, display, actions, update}) => {
+    if (!term.match(/^hotel/)) return;
+
+    actions.open('https://github.com/KELiON/cerebro/blob/master/docs/plugins/plugin-structure.md')
+
+    got(ENDPOINT)
+      .then(pipe( compose(JSON.parse, prop('body'))
+                , serversToList
+                , map(serverToResult({update, actions}))
+                , display
+                ))
+  }
+
+module.exports = {
+  keyword,
+  fn
+}
